3b4646e9987ec00db37150f81916da0e0ffa072e
Android Studio <devandroid@ort>
2023-04-14 12:04:46 -0300

    ej06 finished

--- index.js
+++ index.js
@@ -40,4 +40,7 @@ console.log("----------------------------");
 const objeto = parsearUrl("http://www.ort.edu.ar:8080/alumnos/index.htm?curso=2022&mes=mayo");
 console.log(objeto);
 
+const objeto2 = parsearUrl("ht@tp://www.ort.edu.ar:8080/alumnos/index.htm?curso=2022&mes=mayo");
+console.log(objeto2);
+
 console.log("----------------------------");
--- src/modules/urlParse.js
+++ src/modules/urlParse.js
@@ -1,20 +1,14 @@
 export function parsearUrl(urlString) {
-    const urlParts = urlString.split('?');
-    const urlParams = urlParts[1] ? urlParts[1].split('&') : [];
-    
-    const queryParams = urlParams.reduce((acc, param) => {
-    const [key, value] = param.split('=');
-    acc[key] = value;
-    return acc;
-    }, {});
-    
-    const hostParts = urlParts[0].split('/');
-    const host = hostParts[0] + '//' + hostParts[2];
-    const pathname = urlParts[0].substring(host.length);
-    
-    return {
-        host: host,
-        pathname: pathname,
-        parametros: queryParams
-    };
+    try {
+        const url = new URL(urlString);
+        const queryParams = Object.fromEntries(url.searchParams.entries());
+        return {
+            host: url.origin,
+            pathname: url.pathname,
+            parametros: queryParams
+        };
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
 }
