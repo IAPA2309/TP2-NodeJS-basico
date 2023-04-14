80af357a975be67fa4c563b2d1a8245dc1258879
Android Studio <devandroid@ort>
2023-04-14 11:37:01 -0300

    indexjs prettier

---
+++ index.js
@@ -0,0 +1,35 @@
+import { concatInvert } from "./EJ01.js";
+import { PI, sumar, restar, multiplicar, dividir } from "./src/modules/matematica.js";
+import { Alumno } from "./src/modules/alumno.js";
+// import { copiar } from "./src/modules/EJ04.js";
+
+let textoEntrada01 = "Escuela";
+let textoEntrada02 = "ORT";
+let textoSalida;
+
+
+textoSalida = concatInvert(textoEntrada01, textoEntrada02);
+console.clear();
+console.log("----------------------------");
+console.log(`Textos de Entrada: "${textoEntrada01}" y "${textoEntrada02}"`);
+console.log(`Texto de Salida: "${textoSalida}"`);
+
+console.log("----------------------------");
+
+console.log(`El valor de PI es : ${PI}`);
+console.log(`El resultado de la suma entre 3 y 6 es : ${sumar(3, 6)}`);
+console.log(`El resultado de la resta entre 3 y 6 es : ${restar(3, 6)}`);
+console.log(`El resultado de la multiplicacion entre 3 y 6 es : ${multiplicar(3, 6)}`);
+console.log(`El resultado de la division entre 3 y 6 es : ${dividir(3, 6)}`);
+
+console.log("----------------------------");
+
+let alumno1 = new Alumno("iapa", 2323);
+let alumno2 = new Alumno("vader", 1321123);
+
+console.log(`El alumno 1 es : ${alumno1.mostrar()}`);
+console.log(`El alumno 2 es : ${alumno2.mostrar()}`);
+
+console.log("----------------------------");
+
+// copiar('./entrada.txt', './salida.txt');
---
+++ src/modules/EJ04.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+function copiar(entrada, salida) {
+    fs.readFile(entrada, 'utf8', (error, data) => {
+        if (error) throw error;
+        
+        fs.writeFile(salida, data, (error) => {
+            if (error) throw error;
+        
+            console.log(`El archivo ${entrada} ha sido copiado con éxito como ${salida}.`);
+        });
+    });
+}
+
+module.exports = Copiar;
