d216778901f3e1970f3a65c1cdb1a0fd4645bf5b
IAPA2309 <iniaki.perezaviles@gmail.com>
2023-04-27 20:33:22 -0300

    delete by id, and bug fixes

--- index-select.js
+++ index-select.js
@@ -2,10 +2,14 @@ import config from './dbconfig.js';
 import sql from 'mssql';
 import PizzaService from './src/services/pizza-services.js';
 
+const pizzaService = new PizzaService();
+
 let pool = await sql.connect(config);
 let result = await pool.request().query("SELECT top 2 * FROM Pizzas");
-console.log(PizzaService.getById(1));
+
+result = await pizzaService.getById(1);
+result = await pizzaService.deleteById(2);
+
 
 console.log(result);
-
 process.exit();
--- src/services/pizza-services.js
+++ src/services/pizza-services.js
@@ -1,16 +1,35 @@
+import { config } from "dotenv";
+import sql from 'mssql';
+
 export default class PizzaService {
-    getById = async (id) => {
-        let returnEntity = null;
-        console.log('Estoy en: PizzaService.GetById(id)');
-        try{
-            let pool = await sql.connect(config);
-            let result = await sql.request()
-            .input('pId', sql.Int, id)
-            .query('SELECT * FROM Pizzas WHERE id = @pId');
-            returnEntity = result.recordsets[0][0];
-        }catch(error) {
-            console.log(error);
-        }
-        return returnEntity;
+  getById = async (id) => {
+    let returnEntity = null;
+    console.log("Estoy en: PizzaService.GetById(id)");
+    try {
+      let pool = await sql.connect(config);
+      let result = await pool
+        .request()
+        .input("pId", sql.Int, id)
+        .query("SELECT * FROM Pizzas WHERE id = @pId");
+      returnEntity = result.recordsets[0][0];
+    } catch (error) {
+      console.log(error);
     }
+    return returnEntity;
+  }
+  deleteById = async (id) => {
+    let rowsAffected = 0;
+    console.log(`Estoy en: PizzaService.deleteById(id)`);
+    try {
+      let pool = await sql.connect(config);
+      let result = await pool
+        .request()
+        .input("pId", sql.Int, id)
+        .query("DELETE FROM Pizzas WHERE id = @pId");
+      rowsAffected = result.rowsAffected;
+    } catch (error) {
+      console.log(error);
+    }
+    return rowsAffected;
+  };
 }
